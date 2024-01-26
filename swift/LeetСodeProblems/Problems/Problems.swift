7e1baf5b65f5ee3bcd98834e88391339d2ba2736
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2024-01-26 21:52:27 +0300

    added: library and unit tests targets;

---
+++ swift/LeetСodeProblems/Problems/Problems.swift
@@ -0,0 +1,8 @@
+//
+//  Problems.swift
+//  Problems
+//
+//  Created by Aynur Nasybullin on 26.01.2024.
+//
+
+final class Problems { }
---
+++ swift/LeetСodeProblems/ProblemsTests/ProblemsTests.swift
@@ -0,0 +1,10 @@
+//
+//  ProblemsTests.swift
+//  ProblemsTests
+//
+//  Created by Aynur Nasybullin on 26.01.2024.
+//
+
+import XCTest
+
+final class ProblemsTests: XCTestCase { }
