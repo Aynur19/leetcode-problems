1634ff9e8efa56aa267b0f9425e31f985bd1b89c
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2024-01-28 21:00:49 +0300

    added: solution of problem 228 (swift);

---
+++ swift/LeetСodeProblems/Problems/Problem_228.swift
@@ -0,0 +1,71 @@
+//
+//  Problem_228.swift
+//  LeetСodeProblems
+//
+//  Created by Aynur Nasybullin on 28.01.2024.
+//
+
+//  228. Summary Ranges
+//  Easy
+//  Topics: Array
+
+//  You are given a sorted unique integer array nums.
+//
+//  A range [a,b] is the set of all integers from a to b (inclusive).
+//
+//  Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+//  That is, each element of nums is covered by exactly one of the ranges,
+//  and there is no integer x such that x is in one of the ranges but not in nums.
+//
+//  Each range [a,b] in the list should be output as:
+//  - "a->b" if a != b
+//  - "a" if a == b
+ 
+//  Example 1:
+//  Input: nums = [0,1,2,4,5,7]
+//  Output: ["0->2","4->5","7"]
+//  Explanation: The ranges are:
+//  - [0,2] --> "0->2"
+//  - [4,5] --> "4->5"
+//  - [7,7] --> "7"
+//
+//  Example 2:
+//  Input: nums = [0,2,3,4,6,8,9]
+//  Output: ["0","2->4","6","8->9"]
+//  Explanation: The ranges are:
+//  - [0,0] --> "0"
+//  - [2,4] --> "2->4"
+//  - [6,6] --> "6"
+//  - [8,9] --> "8->9"
+ 
+//  Constraints:
+//  0 <= nums.length <= 20
+//  -2^31 <= nums[i] <= 2^31 - 1
+//  All the values of nums are unique.
+//  nums is sorted in ascending order.
+
+extension Problems {
+    // Approach: Array
+    // Time complexity: O(n) = 5
+    // Space complexity: O(1) = 15.72
+    static func problem_228_summaryRanges(_ nums: [Int]) -> [String] {
+        if nums.isEmpty { return [] }
+        if nums.count == 1 { return [String(nums[0])] }
+        
+        var result = [String]()
+        var start = nums[0]
+        
+        for idx in 1..<nums.count {
+            if nums[idx] > nums[idx - 1] + 1 {
+                let item = start == nums[idx - 1] ? "\(start)" : "\(start)->\(nums[idx - 1])"
+                result.append(item)
+                start = nums[idx]
+            }
+        }
+        
+        let item = start == nums[nums.count - 1] ? "\(start)" : "\(start)->\(nums[nums.count - 1])"
+        result.append(item)
+        
+        return result
+    }
+}
--- swift/LeetСodeProblems/ProblemsTests/ProblemsTests_250.swift
+++ swift/LeetСodeProblems/ProblemsTests/ProblemsTests_250.swift
@@ -9,10 +9,26 @@ import XCTest
 @testable import Problems
 
 extension ProblemsTests {
-    // MARK: Problem 206. Reverse Linked List
+    // MARK: Problem 228. Summary Ranges
+    func testsProblem_228() throws {
+        lazy var testsData_228: [(nums: [Int], expected: [String])] = {
+            var testsData = [(nums: [Int], expected: [String])]()
+            
+            testsData.append((nums: [0,1,2,4,5,7], expected: ["0->2","4->5","7"]))
+            testsData.append((nums: [0,2,3,4,6,8,9], expected: ["0","2->4","6","8->9"]))
+            
+            return testsData
+        }()
+        
+        for data in testsData_228 {
+            let actual = Problems.problem_228_summaryRanges(data.nums)
+            XCTAssertEqual(actual, data.expected, "nums: \(data.nums)")
+        }
+    }
     
+    // MARK: Problem 206. Reverse Linked List
     func testsProblem_206() throws {
-        lazy var testsData_206: [(head: ListNode?, expected: ListNode?)] = {
+        func testsData_206() -> [(head: ListNode?, expected: ListNode?)] {
             var testsData = [(head: ListNode?, expected: ListNode?)]()
             
             testsData.append((
@@ -26,15 +42,14 @@ extension ProblemsTests {
             testsData.append((head: nil, expected: nil))
             
             return testsData
-        }()
+        }
         
-//        for data in testsData_206 {
-//            let actual = Problems.problem_206_reverseList(data.head)
-//            XCTAssertTrue(ListNode.equalListNodes(actual, data.expected), "head: \(data.head?.description ?? "[]")")
-//        }
+        for data in testsData_206() {
+            let actual = Problems.problem_206_reverseList(data.head)
+            XCTAssertTrue(ListNode.equalListNodes(actual, data.expected), "head: \(data.head?.description ?? "[]")")
+        }
         
-        
-        for data in testsData_206 {
+        for data in testsData_206() {
             let actual = Problems.problem_206_reverseList2(data.head)
             XCTAssertTrue(ListNode.equalListNodes(actual, data.expected), "head: \(data.head?.description ?? "[]")")
         }
