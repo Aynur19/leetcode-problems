097e363fb10eb5300f4e8fea7b360cbf9f44e638
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2024-02-17 21:46:47 +0300

    added: solution of problem 1450 (swift);

---
+++ swift/LeetСodeProblems/Problems/Problem_1450.swift
@@ -0,0 +1,53 @@
+//
+//  Problem_1450.swift
+//  LeetСodeProblems
+//
+//  Created by Aynur Nasybullin on 17.02.2024.
+//
+
+//  1450. Number of Students Doing Homework at a Given Time
+//  Easy
+//  Topics: Array
+
+//  Given two integer arrays startTime and endTime and given an integer queryTime.
+//
+//  The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+//
+//  Return the number of students doing their homework at time queryTime.
+//  More formally, return the number of students where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
+//  Example 1:
+//  Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+//  Output: 1
+//  Explanation: We have 3 students where:
+//      The first student started doing homework at time 1 and finished at time 3 and wasn't doing anything at time 4.
+//      The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
+//      The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
+//
+//  Example 2:
+//  Input: startTime = [4], endTime = [4], queryTime = 4
+//  Output: 1
+//  Explanation: The only student was doing their homework at the queryTime.
+ 
+//  Constraints:
+//  startTime.length == endTime.length
+//  1 <= startTime.length <= 100
+//  1 <= startTime[i] <= endTime[i] <= 1000
+//  1 <= queryTime <= 1000
+
+extension Problems {
+    // Approach: Iteration
+    // Time complexity: O(n) = 3 ms
+    // Space complexity: O(1) = 15.38 MB
+    static func problem_1450_busyStudent(_ startTime: [Int], _ endTime: [Int], _ queryTime: Int) -> Int {
+        var result = 0
+        
+        for idx in startTime.indices {
+            if startTime[idx] <= queryTime && endTime[idx] >= queryTime {
+                result += 1
+            }
+        }
+        
+        return result
+    }
+}
--- swift/LeetСodeProblems/ProblemsTests/ProblemsTests_1500.swift
+++ swift/LeetСodeProblems/ProblemsTests/ProblemsTests_1500.swift
@@ -8,6 +8,28 @@
 import XCTest
 
 final class ProblemsTests_1500: XCTestCase {
+    // MARK: Problem 1450. Number of Students Doing Homework at a Given Time
+    func testsProblem_1450() throws {
+        typealias TestCase = (startTime: [Int], endTime: [Int], queryTime: Int, expected: Int)
+        
+        lazy var testsData_1450: [TestCase] = {
+            var testsData = [TestCase]()
+            
+            testsData.append((startTime: [1,2,3], endTime: [3,2,7], queryTime: 4, expected: 1))
+            testsData.append((startTime: [4], endTime: [4], queryTime: 4, expected: 1))
+            
+            return testsData
+        }()
+        
+        
+        for data in testsData_1450 {
+            let message = "startTime: \(data.startTime); endTime: \(data.endTime); queryTime: \(data.queryTime)"
+            let actual = Problems.problem_1450_busyStudent(data.startTime, data.endTime, data.queryTime)
+            
+            XCTAssertEqual(actual, data.expected, message)
+        }
+    }
+    
     // MARK: Problem 1446. Consecutive Characters
     func testsProblem_1446() throws {
         typealias TestCase = (s: String, expected: Int)
